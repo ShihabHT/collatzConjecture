2b475598f65b7d886dde72c31fbef9f5502052a4
ShihabHT <shihabrahman122@gmail.com>
2021-07-30 15:28:03 +0600

    Setting up turtle window

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # Collatz Conjecture
+import turtle
 
-def getSteps(num):
+turtle.bgcolor("black")
+turtle.Screen().setup(1500, 900)
+turtle.Screen().setworldcoordinates(0, 0, 1500, 900)
+
+t = turtle.Turtle()
+t.setpos(500, 200)
+t.pencolor("pink")
+t.penup()
+
+
+def get_steps(num):
     count = 0
     while num != 1:
         if num % 2 == 0:
@@ -11,12 +22,14 @@ def getSteps(num):
             count += 1
     return count
 
+
 if __name__ == '__main__':
     stepsCount = 0
     for x in range(1, 65537):
-        steps = getSteps(x)
+        steps = get_steps(x)
         if steps == 16:
             stepsCount += 1
             print(x, "takes", steps, "steps")
 
     print("Total 16 steps takes", stepsCount, "numbers")
+    turtle.done()
