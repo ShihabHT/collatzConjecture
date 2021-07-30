36df86b19ba85ceca245165e53b204dd45523f6f
ShihabHT <shihabrahman122@gmail.com>
2021-07-30 21:45:55 +0600

    Added drawing functionality for each steps taken

--- main.py
+++ main.py
@@ -7,32 +7,43 @@ turtle.Screen().setworldcoordinates(0, 0, 1500, 900)
 
 t = turtle.Turtle()
 t.penup()
-t.setpos(500, 200)
-t.pendown()
 t.pencolor("pink")
 t.pensize(5)
-t.forward(200)
 
 
 def get_steps(num):
+    t.setpos(500, 200)
+    t.setheading(90)
+    t.pendown()
     count = 0
     while num != 1:
         if num % 2 == 0:
+            t.right(10)
+            t.forward(20)
             num = num / 2
             count += 1
         else:
+            t.left(10)
+            t.forward(20)
             num = num * 3 + 1
             count += 1
+
+    t.penup()
     return count
 
 
 if __name__ == '__main__':
     stepsCount = 0
-    for x in range(1, 65537):
-        steps = get_steps(x)
-        if steps == 16:
-            stepsCount += 1
-            print(x, "takes", steps, "steps")
+    # for x in range(1, 65537):
+    #     steps = get_steps(x)
+    #     if steps == 16:
+    #         stepsCount += 1
+    #         print(x, "takes", steps, "steps")
 
-    print("Total 16 steps takes", stepsCount, "numbers")
+    stepsTaken = get_steps(64)
+    print(stepsTaken, "steps taken")
+    stepsTaken = get_steps(128)
+    print(stepsTaken, "steps taken")
+
+    # print("Total 16 steps takes", stepsCount, "numbers")
     turtle.done()
